f31e42421bf537c92aa0540e5e3dfd4cc90d1e92
Zavum <dakfunchesS@gmaill.com>
2017-02-18 13:24:53 -0500

    Backup plan code. Averages tap times to vibrate the watch at the averaged interval timing. Added heart icon for the button

--- wear/src/main/java/com/salthacks/salt/MainActivity.java
+++ wear/src/main/java/com/salthacks/salt/MainActivity.java
@@ -1,10 +1,15 @@
 package com.salthacks.salt;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -16,27 +21,39 @@ public class MainActivity extends WearableActivity {
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
 
-    private BoxInsetLayout mContainerView;
+    private RelativeLayout mContainerView;
     private TextView mTextView;
     private TextView mClockView;
 
     private Vibrator vibrator;
+
+    private ImageButton button;
+
+    private int presses;
+    private long pressTimes[];
+
+    private long interval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setAmbientEnabled();
-
-        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
-        mClockView = (TextView) findViewById(R.id.clock);
-
+        pressTimes = new long[8];
+        presses = 0;
+        mContainerView = (RelativeLayout) findViewById(R.id.container);
+        mTextView = (TextView) mContainerView.findViewById(R.id.button_presses);
+        mTextView.setText(Integer.toString(8-presses));
+        mTextView.setTextColor(Color.WHITE);
+        button = (ImageButton) mContainerView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                trackBPM();
+            }
+        });
         vibrator =  (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        long[] vibrationPattern = {0, 500, 50, 300};
-        //-1 - don't repeat
-        final int indexInPatternToRepeat = -1;
-        vibrator.vibrate(vibrationPattern, indexInPatternToRepeat);
+
     }
 
     @Override
@@ -61,13 +78,48 @@ public class MainActivity extends WearableActivity {
         if (isAmbient()) {
             mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
             mTextView.setTextColor(getResources().getColor(android.R.color.white));
-            mClockView.setVisibility(View.VISIBLE);
 
-            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+
+
         } else {
             mContainerView.setBackground(null);
             mTextView.setTextColor(getResources().getColor(android.R.color.black));
-            mClockView.setVisibility(View.GONE);
+
         }
+    }
+
+    private void trackBPM(){
+        if(presses < 8) {
+            pressTimes[presses] = System.currentTimeMillis();
+            ++presses;
+            mTextView.setText(Integer.toString(8-presses));
+            mTextView.setTextColor(Color.WHITE);
+            vibrator.vibrate(250);
+            if(presses > 7){
+                calculateAverage();
+            }
+
+        }else{
+            presses = 0;
+            pressTimes[presses] = System.currentTimeMillis();
+            mTextView.setText(Integer.toString(presses));
+            mTextView.setTextColor(Color.WHITE);
+        }
+    }
+    private void calculateAverage(){
+        long sum = 0;
+        for(int i=0; i < 7; ++i){
+            sum += (pressTimes[i+1] - pressTimes[i]);
+        }
+        interval = sum/8;
+
+        setVibrateInterval();
+    }
+
+    private void setVibrateInterval(){
+        long intervalPattern[] = new long[2];
+        intervalPattern[0] = interval-250;
+        intervalPattern[1] = 250;
+        vibrator.vibrate(intervalPattern,0);
     }
 }

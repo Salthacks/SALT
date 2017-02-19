eb1f59cf0bea56a7385e518babf5add2cbc96944
Max Fresonke <max@fresonke.com>
2017-02-18 20:17:42 -0500

    Added recording code that doesn't work with floats :'(

--- mobile/src/main/java/com/salthacks/salt/MainActivity.java
+++ mobile/src/main/java/com/salthacks/salt/MainActivity.java
@@ -1,13 +1,25 @@
 package com.salthacks.salt;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     static {
@@ -17,42 +29,121 @@ public class MainActivity extends AppCompatActivity {
     public native void processAubio(int hopSize, int sampleRate, float in[]);
 
 
+    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        setButtonHandlers();
+        enableButtons(false);
+        int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+        Log.d("MAXWELL DEBUG", "Min Buffer Size: " + bufferSize);
+        Log.d("MAXWELL DEBUG", "Curr Buffer Size: " + (BufferElements2Rec * BytesPerElement));
+    }
+
+    private void setButtonHandlers() {
+        ((Button) findViewById(R.id.btnStart)).setOnClickListener(btnClick);
+        ((Button) findViewById(R.id.btnStop)).setOnClickListener(btnClick);
+    }
+
+    private void enableButton(int id, boolean isEnable) {
+        ((Button) findViewById(id)).setEnabled(isEnable);
+    }
+
+    private void enableButtons(boolean isRecording) {
+        enableButton(R.id.btnStart, !isRecording);
+        enableButton(R.id.btnStop, isRecording);
+    }
+
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+
+    private void startRecording() {
+
+//        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+//                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+//                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            recorder = new AudioRecord.Builder()
+                    .setAudioSource(MediaRecorder.AudioSource.MIC)
+                    .setAudioFormat(new AudioFormat.Builder()
+                        .setEncoding(AudioFormat.ENCODING_PCM_FLOAT)
+                        .setSampleRate(RECORDER_SAMPLERATE)
+                        .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                        .build())
+                    .build();
+        } else {
+            Log.d("MAXWELL DEBUG", "Samsung no updatey :(");
+        }
+        Log.d("MAXWELL DEBUG", "Recorder State: " + recorder.getState());
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                writeAudioDataToFileJK();
             }
-        });
+        }, "AudioRecorder Thread");
+        recordingThread.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void writeAudioDataToFileJK() {
+        // Write the output audio in byte
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        float sData[] = new float[BufferElements2Rec];
+
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                recorder.read(sData, 0, BufferElements2Rec, AudioRecord.READ_BLOCKING);
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+        }
+    }
+
+    private View.OnClickListener btnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnStart: {
+                    enableButtons(true);
+                    startRecording();
+                    break;
+                }
+                case R.id.btnStop: {
+                    enableButtons(false);
+                    stopRecording();
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
